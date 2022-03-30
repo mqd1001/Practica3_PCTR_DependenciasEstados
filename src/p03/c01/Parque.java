a80a682e57a3d4d926fab8e5566d90c801a85aea
acq1001 <acq1001@alu.ubu.es>
2022-03-30 23:11:40 +0200

    Actualizados constructor y metodo entrarAlParque, clase Parque

--- src/p03/c01/Parque.java
+++ src/p03/c01/Parque.java
@@ -3,91 +3,89 @@ package src.p03.c01;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
-public class Parque implements IParque{
+public class Parque implements IParque {
 
-
-	// TODO 
 	private int contadorPersonasTotales;
 	private Hashtable<String, Integer> contadoresPersonasPuerta;
-	
-	
-	public Parque() {	// TODO
+	private int espacioMax;
+
+	public Parque(int espacioMax) {
 		contadorPersonasTotales = 0;
 		contadoresPersonasPuerta = new Hashtable<String, Integer>();
-		// TODO
+		this.espacioMax = espacioMax;
 	}
-
 
 	@Override
-	public void entrarAlParque(String puerta){		// TODO
-		
+	public synchronized void entrarAlParque(String puerta) {
+		// Hemos anadido el synchronized para que solo pueda entrar un
+		// hilo cada vez
+
 		// Si no hay entradas por esa puerta, inicializamos
-		if (contadoresPersonasPuerta.get(puerta) == null){
+		if (contadoresPersonasPuerta.get(puerta) == null) {
 			contadoresPersonasPuerta.put(puerta, 0);
 		}
-		
-		// TODO
-				
-		
+
+		//Comprobacion 
+		comprobarAntesDeEntrar();
+
 		// Aumentamos el contador total y el individual
-		contadorPersonasTotales++;		
-		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta)+1);
-		
+		contadorPersonasTotales++;
+		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta) + 1);
+
 		// Imprimimos el estado del parque
 		imprimirInfo(puerta, "Entrada");
-		
-		// TODO
-		
-		
-		// TODO
-		
+
+		//Notificamos a todos los hilos
+		this.notifyAll();
+
+		//Comprobamos que el conteo es correcto
+		checkInvariante();
+
 	}
-	
-	// 
+
+	//
 	// TODO Método salirDelParque
 	//
-	
-	
-	private void imprimirInfo (String puerta, String movimiento){
+
+	private void imprimirInfo(String puerta, String movimiento) {
 		System.out.println(movimiento + " por puerta " + puerta);
-		System.out.println("--> Personas en el parque " + contadorPersonasTotales); //+ " tiempo medio de estancia: "  + tmedio);
-		
+		System.out.println("--> Personas en el parque " + contadorPersonasTotales); // + " tiempo medio de estancia: " +
+																					// tmedio);
+
 		// Iteramos por todas las puertas e imprimimos sus entradas
-		for(String p: contadoresPersonasPuerta.keySet()){
+		for (String p : contadoresPersonasPuerta.keySet()) {
 			System.out.println("----> Por puerta " + p + " " + contadoresPersonasPuerta.get(p));
 		}
 		System.out.println(" ");
 	}
-	
+
 	private int sumarContadoresPuerta() {
 		int sumaContadoresPuerta = 0;
-			Enumeration<Integer> iterPuertas = contadoresPersonasPuerta.elements();
-			while (iterPuertas.hasMoreElements()) {
-				sumaContadoresPuerta += iterPuertas.nextElement();
-			}
+		Enumeration<Integer> iterPuertas = contadoresPersonasPuerta.elements();
+		while (iterPuertas.hasMoreElements()) {
+			sumaContadoresPuerta += iterPuertas.nextElement();
+		}
 		return sumaContadoresPuerta;
 	}
-	
+
 	protected void checkInvariante() {
-		assert sumarContadoresPuerta() == contadorPersonasTotales : "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parte";
-		// TODO 
+		assert sumarContadoresPuerta() == contadorPersonasTotales
+				: "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parte";
 		// TODO
-		
-		
-		
+		// TODO
+
 	}
 
-	protected void comprobarAntesDeEntrar(){	// TODO
-		//
-		// TODO
-		//
-	}
-
-	protected void comprobarAntesDeSalir(){		// TODO
+	protected void comprobarAntesDeEntrar() { // TODO
 		//
 		// TODO
 		//
 	}
 
+	protected void comprobarAntesDeSalir() { // TODO
+		//
+		// TODO
+		//
+	}
 
 }
