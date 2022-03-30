aed1017d4c8245068114532d805634aac89cf116
María Teresa Quintanal <maite.quintanal@gmail.com>
2022-03-30 22:54:10 +0200

    Actualizar sistemaLanzador

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -6,13 +6,13 @@ public class SistemaLanzador {
 		IParque parque = new Parque(); // TODO
 		char letra_puerta = 'A';
 		
-		System.out.println("Â¡Parque abierto!");
+		System.out.println("¡Parque abierto!");
 		
 		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
 			
 			String puerta = ""+((char) (letra_puerta++));
 			
-			// CreaciÃ³n de hilos de entrada
+			// Creación de hilos de entrada
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
 			new Thread (entradas).start();
 			
